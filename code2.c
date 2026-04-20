57dcb95f5d50d3c4bb3402000fc28195bfbe46b5
SamikshaBhaisare <samikshabhaisare8@gmail.com>
2026-04-20 06:52:14 +0530

    Combination using Function

--- code2.c
+++ code2.c
@@ -1,38 +1,19 @@
 #include<stdio.h>
-int sum(int a,int b){
-    int sum=a+b;
-    printf("sum is: %d\n",sum);
-    return 0;
-
-}
-int div(int a,int b){
-    int div=a/b;
-    printf("div is: %d\n",div);
-    return 0;
-
-}
-int sub(int a,int b){
-    int sub=a-b;
-    printf("sub is: %d\n",sub);
-    return 0;
-
-}
-int mul(int a,int b){
-    int mul=a*b;
-    printf("mul is: %d\n",mul);
-    return 0;
-
+int factorial(int x){
+    int fact=1;
+    for(int i=2;i<=x;i++){
+        fact=fact*i;
+    }
+    return fact;
 }
 int main(){
-int a;
-    printf("enter the value of a:");
-    scanf("%d",&a);
-    int b;
-    printf("enter the value of b:");
-    scanf("%d",&b);
-    sum(a,b);
-    div(a,b);
-    sub(a,b);
-    mul(a,b);
-
+    int n;
+    printf("Enter the value of n:");
+    scanf("%d",&n);
+     int r;
+    printf("Enter the value of r:");
+    scanf("%d",&r);
+    int nCr=factorial(n)/(factorial(r)*factorial(n-r));
+    printf("The vaule of nCr is:%d\n",nCr);
+    return 0;
 }
