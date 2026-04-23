bc74c755ed32e7112303a581b8048999b34196ac
SamikshaBhaisare <samikshabhaisare8@gmail.com>
2026-04-23 09:44:44 +0530

    compare two structure

--- code10.c
+++ code10.c
@@ -2,24 +2,22 @@
 struct student{
     char name[50];
     int marks;
-
 };
 int main(){
     struct student s1,s2;
-    printf("Enter the name of student1:");
+    printf("Enter the student name 1:");
     scanf("%s",s1.name);
-     printf("Enter the marks of student1:");
+    printf("Enter the student marks 1:");
     scanf("%d",&s1.marks);
-    printf("Enter the name of student2:");
+     printf("Enter the student name 2:");
     scanf("%s",s2.name);
-     printf("Enter the marks of student2:");
+    printf("Enter the student marks 2:");
     scanf("%d",&s2.marks);
     if(s1.marks>s2.marks){
-        printf("%s Highest score  %d \n",s1.name,s1.marks);
+        printf("%s highest score is:%d\n",s1.name,s1.marks);
     }
     else{
-         printf("%s Highest score %d \n",s2.name,s2.marks);
+        printf("%s highest score is:%d\n",s2.name,s2.marks);
     }
     return 0;
-
 }
