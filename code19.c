c05faa57870faf8cfb9b4e6a433abbd76953ec20
SamikshaBhaisare <samikshabhaisare8@gmail.com>
2026-05-05 10:54:54 +0530

    call by reference

---
+++ code19.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void swap(int* a,int* b);
+int main(){
+    int a=2,b=6;
+    swap(&a,&b);
+    printf("a=%d & b=%d\n",a,b);
+    return 0;
+}
+void swap(int* a,int* b){
+    int t=*a;
+    *a=*b;
+    *b=t;
+   
+}
