a291b25ae21f75309a923b883066b71172051298
SamikshaBhaisare <samikshabhaisare8@gmail.com>
2026-05-31 16:18:37 +0530

    Set a Bit

---
+++ code56.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int n,P1;
+    printf("Enter value of P1:");
+    scanf("%d",&P1);
+    printf("Enter value of n:");
+    scanf("%d",&n);
+    P1=P1|(1<<n);
+    printf("Set bit is:%d",P1);
+    return 0;
+}
